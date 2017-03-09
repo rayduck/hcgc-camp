35d4ac42324efff3f3f9e9d746538d89a803e697
rootkid <rkxuanfeng@gmail.com>
2017-03-09 22:33:47 +0800

    Minor fixes to improve readability

--- src/components/SchoolPicker.js
+++ src/components/SchoolPicker.js
@@ -5,7 +5,7 @@ class SchoolPicker extends React.Component {
     return (
       <form className='school-selector' onSubmit={this.goToSchool}>
         <h2>Please Enter A School</h2>
-        <input type='text'required placeholder='School code' defaultValue='Enter your school code' ref={(input) => { this.SchoolInput = input }} />
+        <input type='text'required placeholder='Enter your School code' ref={(input) => { this.SchoolInput = input }} />
         <button type='submit'>Signup</button>
       </form>
     )
