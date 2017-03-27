0df1867b9c60106d7f5291aabb65cc2916ddcb17
rayduck <ruida@dingansi.ch>
2017-03-27 19:25:36 +0800

    edits School.js to retrieve schoolId

--- src/components/School.js
+++ src/components/School.js
@@ -5,31 +5,23 @@ import base from '../base'
 import StudentInfo from './StudentInfo'
 import Panel from './Panel'
 
+/*
 class School extends Component {
-  constructor () {
-    super()
-    this.state = {
-      sid: null
-    }
-  }
   render () {
     return (
       <div className='School'>
         <h2>This is the School participants portion</h2>
-        <h3>School is {this.props.match}</h3>
         <StudentInfo />
         <Panel />
       </div>
     )
   }
 }
-
-/*
+*/
 const School = ({match}) => (
   <div>
     <h3>school is {match.params.schoolId}</h3>
   </div>
 )
-*/
 
 export default School
