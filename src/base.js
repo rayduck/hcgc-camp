37a7ba827659234f4892237921b31499a1c72fe4
rayduck <ruida@dingansi.ch>
2017-03-29 23:06:01 +0800

    updates Rebase to use hcgcgreencamp firebase

--- src/base.js
+++ src/base.js
@@ -2,9 +2,9 @@
 import Rebase from 're-base'
 
 const base = Rebase.createClass({
-  apiKey: 'AIzaSyAIH3M2La_YcIhZ0ROGAowLus1Xhx0ThHY',
-  authDomain: 'hcgc-5d76f.firebaseapp.com',
-  databaseURL: 'https://hcgc-5d76f.firebaseio.com'
+  apiKey: 'AIzaSyAIH3M2La_YcIhZ0ROGAowLus1Xhx0ThH',
+  authDomain: 'hcgcgreencamp-6a522.firebaseapp.com',
+  databaseURL: 'https://hcgcgreencamp-6a522.firebaseio.com'
 })
 
 export default base
